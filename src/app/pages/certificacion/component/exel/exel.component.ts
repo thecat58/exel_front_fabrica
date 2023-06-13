b70d18669d65a810c7eac549c610cd4b2a28d76e
thecat58 <mkike518@gmail.com>
2023-06-13 08:24:07 -0500

    ya esta ahora as el pdf

--- src/app/pages/certificacion/component/exel/exel.component.ts
+++ src/app/pages/certificacion/component/exel/exel.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CertificacionService } from '@services/certificacion.service'
+import { UINotificationService } from '@services/uinotification.service';
+
 
 @Component({
   selector: 'app-exel',
@@ -7,7 +9,34 @@ import { CertificacionService } from '@services/certificacion.service'
   styleUrls: ['./exel.component.scss']
 })
 export class ExelComponent {
+  progreso: number = 0;
+  cargandoArchivo = false;
 
+  constructor(
+    private _uiNotificationService: UINotificationService,
+    private _importarExcel: CertificacionService,
+       
+   ) {}
+
+
+   importarDocumetosMora(event) {
+    event.preventDefault();
+    this.cargandoArchivo = true;
+    const archivo = event.target.documento.files[0];
+    this._importarExcel.importarExcel(archivo).subscribe(
+      response => {
+        console.log(response);
+        this._uiNotificationService.success("Ok! importación completada, ejecute el procedimiento");
+        this.cargandoArchivo = false;
+
+      },
+      error => {
+        console.log(error);
+        this._uiNotificationService.error("Upps! Ocurrio un error");
+        this.cargandoArchivo = false;
+      }
+    );
+  }
 
  
 
